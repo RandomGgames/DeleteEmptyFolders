b5acb7dc5b0a48f5c6926ccde52b1af3c2d29e64
Gianluca O <gianopp@gmail.com>
2025-08-31 15:50:23 -0400

    Update delete_empty_folders.py

--- delete_empty_folders.py
+++ delete_empty_folders.py
@@ -47,8 +47,11 @@ def path_is_ignored(path: str, ignore_these_exact_paths: typing.List[str], any_p
     return False
 
 
-def dir_is_empty(directory) -> bool:
-    for _, _, files in os.walk(directory):
+def dir_is_empty(path) -> bool:
+    if not os.path.isdir(path):
+        logger.debug(f"Path is not a directory")
+        return False
+    for _, _, files in os.walk(path):
         if files:
             logger.debug(f"Dir is not empty")
             return False
